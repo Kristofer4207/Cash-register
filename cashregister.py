2795936aca7de9b84d263c34ee7bbad091b49950
Kristofer4207 <56038623+Kristofer4207@users.noreply.github.com>
2019-11-08 09:04:24 -0700

    Add files via upload

---
+++ cashregister.py
@@ -0,0 +1,17 @@
+# variables 
+dollars = 0 
+halfDollars = 0
+quarters = 0 
+dimes = 0 
+nickels = 0 
+pennies = 0
+price = 0
+change = 0
+given = 0
+
+# inputs 
+price = float(input("What is the price? "))
+given = float(input("How much are you paying with? "))
+ 
+float(given) - float(price) = float(change)
+print("Your change is" + change)
